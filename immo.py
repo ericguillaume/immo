6c1bb1be00c5768a8f1828f480987af112fe05de
Eric Guillaume <eric@zen.ly>
2019-11-05 15:59:26 +0100

    PROBLEM WITH LIN REG SANNOIS

--- immo.py
+++ immo.py
@@ -1,48 +1,54 @@
-import matplotlib
-import numpy as np
-import pandas as pd
+import argparse
+
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.linear_model import LinearRegression
 
-from tools import load_data, surface_under_30, surface_between_30_and_45, surface_above_45
+from data import notaire_sales
 
 
 '''
     todo:
+        * modeliser le tout avec des fonctions mathematiques forcees en simplifiant les dependences
+            et faire un RN qui apprend, par exemple price_m2 = f(surface) * f(etage) * f(age)
         * faire features sur age du logement et prix du m2
         * trouver moyen de mettre SOM dessus
         
         date of construction can be nan
 '''
 
+# additionnal features # "rdc", "garage", "cave"]
+LINEAR_REG_CITY_FEATURES = {
+    "default": ["garage"],
+    "issy": ["garage", "rdc", "cave"],
+}
 
-def linear_reg(data):
-    data = data.dropna()
-    data = data[data["new"] == 0]
-    # data = data[data["age"] <= 2020 - 1996]
-    data = data[(data["age"] >= 10) & (data["age"] <= 40)]
-    print(data)
+LINEAR_REG_CITY_KWARGS = {
+    "default": {"age_min": 10.0, "age_max": 20.0}
+}
 
-    # test
+
+def linear_reg(city, **kwargs):
+    kwargs.update(LINEAR_REG_CITY_KWARGS.get(city, LINEAR_REG_CITY_KWARGS["default"]))
+
+    data = notaire_sales(city, **kwargs)
+    # print(data)
+
+    features = ["surface_under_30", "surface_between_30_and_45", "surface_above_45"]
+    features += LINEAR_REG_CITY_FEATURES.get(city, LINEAR_REG_CITY_FEATURES["default"])
+
     x = []
     y = []
-    features = ["surface_under_30",
-                "surface_between_30_and_45",
-                "surface_above_45",
-                "garage"]
-    # "rdc",
-    # "cave"]  # "rdc", "garage", "cave"]
     for index, row in data.iterrows():
-        new_row = [row[feat] for feat in features]
-        x.append(new_row)
+        x.append([row[feat] for feat in features])
         y.append(row["price"])
     x = np.array(x)
     y = np.array(y)
 
     reg = LinearRegression(fit_intercept=False).fit(x, y)
+    print("{} linear_reg       --".format(city, kwargs))
     print("features = {}".format(features))
     print("reg.coef_ = {}".format(reg.coef_))
-    print("reg.intercept_ = {}".format(reg.intercept_))
 
 
 def histogram_old_prices_for_all_pieces(data):
@@ -53,42 +59,48 @@ def histogram_old_prices_for_all_pieces(data):
         plt.show()
 
 
-def price_3_pieces_by_age(data):
-    data = data[data["pieces"] == 3]
-    data.groupby("date_construction")[["price_per_m2"]].mean().plot()
-    plt.show()
-
-
 def old_2_pieces_price_sorted_by_date(data):
     data = data[(data["pieces"] == 2) & (data["new"] == 0)]
     print(data.sort_values(by="date_construction"))
 
 
-def run():
-    data = load_data()
-    data = pd.DataFrame(data)
-    data["price_per_m2"] = data["price"] / data["surface"]  # faire average de age par categorie.... ajouter age
-    data["surface_under_30"] = data["surface"].apply(surface_under_30)
-    data["surface_between_30_and_45"] = data["surface"].apply(surface_between_30_and_45)
-    data["surface_above_45"] = data["surface"].apply(surface_above_45)
-    data = data.dropna()
+# compares globally old and new real estate
+def compare_old_and_new(city):
+    # pandas.DataFrame.hist() aller voir code pour comprendre pk affiche pas clairement
+    old_data = notaire_sales(city, dropna=True, old_only=True, split_surface=False)
+    print("{} histogram_features_for_old_and_new".format(city))
+    print(old_data.groupby("pieces").mean())
 
-    # data_old = data[data["new"] == 0]
-    # print(data_old.groupby("pieces").mean())
-    #
-    # data_new = data[data["new"] == 1]
-    # print(data_new.groupby("pieces").mean())
+    new_data = notaire_sales(city, dropna=True, new_only=True, split_surface=False)
+    print("{} histogram_features_for_new".format(city))
+    print(new_data.groupby("pieces").mean())
 
-    # data.hist(["price"], bins=30)
-    # plt.show()
+    # display histograms prices
+    fig, (old_ax, new_ax) = plt.subplots(ncols=2, figsize=(10, 8), sharex=True, sharey=True)
 
-    # data.hist("surface", bins=30)
-    # plt.show()
+    old_ax.set_title("{}: histogram old prices".format(city))
+    old_data.hist(["price"], bins=30, ax=old_ax, label="ttt")
 
-    # data = data.drop("surface", axis=1)
+    new_ax.set_title("{}: histogram new prices".format(city))
+    new_data.hist(["price"], bins=30, ax=new_ax)
+    plt.show()
 
-    linear_reg(data)
+
+def price_old_3_pieces_by_age(city):
+    print("{} prices of 3 pieces by age".format(city))
+    data = notaire_sales(city, dropna=True, old_only=True, pieces_count=3)
+    data.groupby("date_construction")[["price_per_m2"]].mean().plot()  # mettre dans plot
+    plt.show()
+
+
+def run(city):
+    # price_old_3_pieces_by_age(city)
+    # compare_old_and_new(city)
+    linear_reg(city, dropna=True, old_only=True)
 
 
 if __name__ == "__main__":
-    run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--city', default="sannois")
+    args = parser.parse_args()
+    run(args.city)
--- tools.py
+++ tools.py
@@ -1,7 +1,4 @@
-import csv
 import re
-
-import numpy as np
 
 
 def surface_under_30(surface):
@@ -21,35 +18,6 @@ def surface_between(surface, lower_bound, upper_bound):
     return max(min(surface, upper_bound - lower_bound), 0)
 
 
-def load_data():
-    rows = []
-    with open("/Users/eguillaume/Downloads/Ventes-d--Issy-les-Moulineaux--92-03112019.csv") as f:
-        reader = csv.reader(f, delimiter=";")
-
-        for idx, row in enumerate(reader):
-            if idx <= 2 or len(row[0]) == 0:
-                continue
-            try:
-                row = {"price": float(numeric_only(row[0])),
-                       "surface": float(numeric_only(row[1])),
-                       "pieces": int(numeric_only(row[2])),
-                       "new": 1 if row[3] == "2011 / 2020" else 0,
-                       "date_construction": csv_string_to_age(row[3]),
-                       "garage": 1 if row[4] == "Oui" else 0,
-                       "rdc": 1 if int(numeric_only(row[7])) == 0 else 0,
-                       "cave": 1 if row[8] == "Oui" else 0}  # cando ajouter les balcons exclue les -
-                if row["surface"] == 0.0:
-                    continue  # trouver toutes ces extremess
-                row["age"] = 2020 - row["date_construction"]
-            except ValueError as e:
-                # print(row)
-                # print(e)
-                continue
-
-            rows.append(row)
-    return rows
-
-
 def alphanum(string):
     pattern = re.compile(r"[\W_]+", re.ASCII)
     return pattern.sub("", string)
@@ -58,33 +26,6 @@ def alphanum(string):
 def numeric_only(string):
     pattern = re.compile(r"[a-z\W_]+", re.ASCII)
     return pattern.sub("", string)
-
-
-def csv_string_to_age(value):
-    # if value == "-" return nan et exclue
-    if value == "-":
-        return np.nan
-    if value == "2011 / 2020":
-        return 2016
-    elif value == "2001 / 2010":
-        return 2006
-    elif value == "1992 / 2000":
-        return 1996
-    elif value == "1981 / 1991":
-        return 1986
-    elif value == "1970 / 1980":
-        return 1975
-    elif value == "1948 / 1969":
-        return 1958
-    elif value == "1914 / 1947":
-        return 1930
-    elif value == "1850 / 1913":
-        return 1881
-    elif value == "Avant 1850":
-        return 1830
-    else:
-        print("csv_string_to_age dont support {}".format(value))
-        exit()
 
 
 
